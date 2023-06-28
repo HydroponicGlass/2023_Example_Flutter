d50b57ba9135f769f0b39a94233595510854a5fe
HydroponicGlass <hydroponicglass@gmail.com>
2023-06-28 20:50:45 +0900

    [list_tile] list_tile complete

---
+++ list_tile/lib/list_tile_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// ref : https://velog.io/@sharveka_11/Flutter-4.-ListTile
+
+class ListTileWidget extends StatelessWidget {
+  List<String> dummy_list = ['aaa', 'bbb', 'ccc'];
+
+  void _listTileOnTap(String value) {
+    print(value);
+  }
+
+  void _listTileIconOnTap(String value) {
+    print(value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: dummy_list.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            elevation: 0,
+            color: Colors.lightBlueAccent,
+            child: ListTile(
+              title: Text(dummy_list[index], style: TextStyle(color: Colors.white),),
+              subtitle: Text(dummy_list[index]),
+              trailing: GestureDetector(
+                  onTap: () =>
+                      _listTileIconOnTap("listtileicon : " + dummy_list[index]),
+                  child: Icon(Icons.add)),
+              onTap: () => _listTileOnTap("listtile : " + dummy_list[index]),
+            ),
+          );
+        });
+  }
+}
